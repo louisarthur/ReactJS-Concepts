e14aaedbda40a94c7a1d94bc2c33fce904a928fe
Louis Arthur <louisartur.53@gmail.com>
2020-05-13 22:00:31 -0300

    ending the project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+      console.log(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: "Umbriel",
+      url: "https://github.com/Rocketseat/umbriel",
+      techs: ["Node", "Express", "TypeScript"],
+    });
+    // Interessante a resposta do dado postado, pois aqui ele utiliza como novo repositories
+    // console.log(response.status);
+    // console.log(response.statusText);
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+    const newRepositories = repositories.filter((repo) => {
+      return repo.id !== id;
+    });
+    setRepositories(newRepositories);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repo) => {
+          return (
+            <li key={repo.id}>
+              {repo.title}
+              <button
+                type="button"
+                onClick={() => handleRemoveRepository(repo.id)}
+              >
+                {" "}
+                Remover{" "}
+              </button>
+            </li>
+          );
+        })}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
